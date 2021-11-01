3d609eb6c3bcae2adf1abdcf2b1b507c73575a99
jmParkGit <papk0904@naver.com>
2021-11-01 21:44:17 +0900

    update likePage,likecard to use detail and remove DB

--- components/LikeCard.js
+++ components/LikeCard.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet,TouchableOpacity} from 'react-native'
+import {View, Image, Text, StyleSheet,TouchableOpacity, ScrollView,Alert} from 'react-native'
+import {firebase_db} from "../firebaseConfig";
+import Constants from 'expo-constants';
 
 //MainPage로 부터 navigation 속성을 전달받아 Card 컴포넌트 안에서 사용
-export default function LikeCard({content,navigation}){
+export default function LikeCard({content,navigation,reload}){
+
+      const remove = () => {
+        console.log("remove function")
+        const user_id = Constants.installationId;
+        firebase_db.ref('/like/'+user_id+'/'+content.idx).remove().then(function(){
+            Alert.alert("삭제 완료");
+            reload();
+        })
+    }
+
     return(
         //카드 자체가 버튼역할로써 누르게되면 상세페이지로 넘어가게끔 TouchableOpacity를 사용
         <View style={styles.card}>
@@ -11,6 +23,10 @@ export default function LikeCard({content,navigation}){
                 <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
                 <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
                 <Text style={styles.cardDate}>{content.date}</Text>
+                <ScrollView style={styles.buttonContainer} horizontal indicatorStyle={"white"}>
+                  <TouchableOpacity style={styles.middleButton04} onPress={()=>{navigation.navigate('DetailPage',{idx:content.idx})}}><Text style={styles.cardButton_0}>자세히보기</Text></TouchableOpacity>
+                  <TouchableOpacity style={styles.middleButton04} onPress={()=>remove()}><Text style={styles.cardButton_0}>찜 해제</Text></TouchableOpacity>
+                </ScrollView>
             </View>
         </View>
     )
@@ -48,5 +64,20 @@ const styles = StyleSheet.create({
     cardDate: {
       fontSize:10,
       color:"#A6A6A6",
-    }
+    },
+    buttonContainer: {
+
+    },
+    cardButton_0: {
+      width:100,
+      height:50,
+      padding:15,
+      color:"#f886a8",
+      borderColor:"#f886a8",
+      borderWidth:2,
+      borderRadius:15,
+      margin:7,
+      textAlign:"center"
+    },
+
 });
